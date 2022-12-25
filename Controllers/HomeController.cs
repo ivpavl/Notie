54c9bc4852369257d07d276353999b6bed05ea71
d <d>
2022-12-25 02:14:06 +0200

    Created some template for future usage

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,8 +6,15 @@ namespace Notie.Controllers;
 
 public class HomeController : Controller
 {
+
     private readonly ILogger<HomeController> _logger;
 
+    List<TaskModel> TestTasks = new List<TaskModel> 
+    {
+        new TaskModel(25, "cha", "he"),
+        new TaskModel(25, "bha", "he22"),
+        new TaskModel(5, "aha", "he8987")
+    };
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
@@ -21,6 +28,10 @@ public class HomeController : Controller
     public IActionResult Privacy()
     {
         return View();
+    }
+    public IActionResult Tasks()
+    {
+        return View(TestTasks);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Models/TaskModel.cs
@@ -0,0 +1,8 @@
+namespace Notie.Models;
+
+public record class TaskModel(int Id, string Name, string Description);
+// {
+//     public int Id;
+//     public string Name;
+//     public string Description;
+// }
